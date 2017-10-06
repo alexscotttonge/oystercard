98ce4bec3724773a2cf68435eafe66449fb67e49
Alex <alex@scotttonge.com>
2017-10-06 10:21:51 +0100

    Added passing unit test for creating the station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,14 @@
+require 'station'
+
+describe Station do
+
+  subject (:station) {Station.new}
+
+  it 'knows its name' do
+    expect(station.name).to eq 'Stamford Brook'
+  end
+
+  it 'knows its zone' do
+    expect(station.zone).to eq 2
+  end
+end
